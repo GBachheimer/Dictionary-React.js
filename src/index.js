c68d4ad7321f78f1ffce1d3a1e5df08c2b9d2aed
GBachheimer <gbachheimer@icloud.com>
2022-08-11 14:16:15 +0300

    modify components, make them reusable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Dictionary from './components/Dictionary';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
